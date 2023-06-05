0f34149d70c349d40d972ec0c46c0cd7970647b6
Varsha Manchani <121595056+manchanivarsha@users.noreply.github.com>
2023-06-05 22:26:45 +0530

    added js

---
+++ usercard2.js
@@ -0,0 +1,29 @@
+let dhonioldinfo={
+    imgurl: "https://images.indiafantasy.com/wp-content/uploads/20220724002346/i.jpeg",
+    name: "Dhoni",
+    Description: "train"
+}
+let dhoniyounginfo={
+    imgurl: "https://images.indiafantasy.com/wp-content/uploads/20220724002346/i.jpeg",
+    name: "M.S.Dhoni",
+    Description: "indian crickter"
+
+
+}
+let isdhoniold=true;
+let displayobject;
+let flipdata= function(){
+    if(isdhoniold == true)
+    {
+        displayobject=dhoniyounginfo;
+        isdhoniold=false;
+    }
+    else{
+        displayobject=dhonioldinfo;
+        isdhoniold=true;
+    }
+    document.getElementById("img-dhoni").src=displayobject.imgurl;
+    document.getElementById("name-dhoni").innerHTML=displayobject.name;
+    document.getElementById("des-dhoni").inner.HTML=displayobject.Description;
+
+}
