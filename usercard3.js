1e127a9cd61230b3425c15b909f5b5a0ad0a0e52
Varsha Manchani <121595056+manchanivarsha@users.noreply.github.com>
2023-06-13 22:29:21 +0530

    added

--- usercard3.js
+++ usercard3.js
@@ -10,8 +10,7 @@ let getRandomUser= function(){
   fetch("https://randomuser.me/api")
   .then(response =>response.json())
   .then(data=>{
-    // displayobject.name=data.results[0].name.first+" "+data.results[0].name.last
-    
+    displayobject.name=data.results[0].name.first+" "+data.results[0].name.last
     displayobject.imgurl=data.results[0].picture.large
     displayobject.Description=data.results[0].gender
     document.getElementById("img-dhoni").src=displayobject.imgurl;
