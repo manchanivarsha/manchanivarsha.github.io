ef72631deb3f71c8b972373377ff00022b27dfab
Varsha Manchani <121595056+manchanivarsha@users.noreply.github.com>
2023-06-13 21:53:28 +0530

    added json

--- usercard3.js
+++ usercard3.js
@@ -1,23 +1,13 @@
 let dhonioldinfo={
     imgurl: "https://resize.indiatvnews.com/en/resize/newbucket/730_-/2016/09/ms-dhoni-1474525188.jpg",
     name: "Dhoni",
-    Description: "Railways TC"}
-let dhoniyounginfo={
-    imgurl: "https://images.indiafantasy.com/wp-content/uploads/20220724002346/i.jpeg",
-    name: "M.S.Dhoni",
-    Description: "indian crickter"}
-let isdhoniold=true;
+    Description: "Railways TC"
+}
 let displayobject;
-let flipdata= function(){
-    if(isdhoniold == true)
-    {
-        displayobject=dhoniyounginfo;
-        isdhoniold=false;
-    }
-    else{
-        displayobject=dhonioldinfo;
-        isdhoniold=true;
-    }
+let getRandomUser= function(){
+  fetch("https://randomuser.me/api")
+  .then(response =>response.json())
+  .then(data=>console.log(data))
     document.getElementById("img-dhoni").src=displayobject.imgurl;
     document.getElementById("name-dhoni").innerHTML=displayobject.name;
     document.getElementById("des-dhoni").innerHTML=displayobject.Description;
